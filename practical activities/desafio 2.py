a59ea827ea4ac5a921c884f7079794dd521563d3
jean <jeanmarlon037@gmail.com>
2024-12-19 15:49:01 -0300

    Delete practical activities directory
    updates

--- practical activities/desafio 2.py
+++
@@ -1,4 +0,0 @@
-dia=input('Dia = ')
-mes=input('Mes = ')
-ano=input('Ano = ')
-print('você nasceu no dia', dia, 'de', mes, 'de', ano,'. ' 'Correto?')
--- practical activities/desafio 3.py
+++
@@ -1,5 +0,0 @@
-print('calculadora de soma')
-num1=input('Primeiro número ')
-num2=input('Segundo número ')
-res=num1+num2
-print('A soma é', res)
