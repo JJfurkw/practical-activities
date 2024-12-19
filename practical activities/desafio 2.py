864365e9e63c3d2003d17fec5c2a0915c015abb6
jean <jeanmarlon037@gmail.com>
2024-12-19 13:47:01 -0300

    Add files via upload

---
+++ practical activities/desafio 2.py
@@ -0,0 +1,4 @@
+dia=input('Dia = ')
+mes=input('Mes = ')
+ano=input('Ano = ')
+print('você nasceu no dia', dia, 'de', mes, 'de', ano,'. ' 'Correto?')
---
+++ practical activities/desafio 3.py
@@ -0,0 +1,5 @@
+print('calculadora de soma')
+num1=input('Primeiro número ')
+num2=input('Segundo número ')
+res=num1+num2
+print('A soma é', res)
